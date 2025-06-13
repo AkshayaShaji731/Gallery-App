21097dc0253b84c4746074e1cf5046ca1d28dee1
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-13 14:33:03 +0530

    card data added from json placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
-<<<<<<< HEAD
   <search />
+  <card />
 </template>
 <script setup>
 import search from "@/components/search.vue";
+import card from "@/components/cardSection.vue";
 </script>
 <style>
 body {
   background-color: #111827;
   box-sizing: border-box;
-=======
- <card/>
-</template>
-<script setup>
-import card from '@/components/cardSection.vue'
-</script>
-<style>
-body{
-  background-color: #111827;
->>>>>>> 90bb5bd (card ui created)
 }
 </style>
--- src/components/cardSection.vue
+++ src/components/cardSection.vue
@@ -1,67 +1,86 @@
 <template>
-    <section class="main-container">
-        <section class="card">
-            <div class="img-container">
-                <img src="https://pbs.twimg.com/profile_images/1701878932176351232/AlNU3WTK_400x400.jpg" alt="img">
-            </div>
-            <div class="text-container">
-                <span class="title">helo</span>
-                <span class="id">hiii</span>
-            </div>
-        </section>
-        <section class="card">
-            <div class="img-container">
-                <img src="https://pbs.twimg.com/profile_images/1701878932176351232/AlNU3WTK_400x400.jpg" alt="img">
-            </div>
-            <div class="text-container">
-                <span class="title">helo</span>
-                <span class="id">hiii</span>
-            </div>
-        </section>
+  <section class="main-container">
+    <section  v-for="post in postPhoto" :key="post.id" class="card">
+      <div class="img-container">
+        <img src="https://fastly.picsum.photos/id/488/200/200.jpg?hmac=V8mvdG1ON09kNw80-qS00BSFq5gGhqRYoYPJftrsYA8"
+          alt="img"
+        />
+      </div>
+      <div class="text-container">
+        <span class="title">{{ post.title }}</span>
+        <span class="id">{{ post.id }}</span>
+      </div>
     </section>
+  </section>
 </template>
-<script>
 
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      postPhoto: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => (this.postPhoto = response.data))
+  },
+};
 </script>
+
 <style>
-.main-container{
-    display: grid;
-    grid-template-columns:repeat(2,1fr);
-    padding-inline:16px;
-    gap: 16px;
-    margin-top:30px
+.main-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding-inline: 16px;
+  gap: 16px;
+  margin-top: 30px;
 }
-.card{
-    display: flex;
-    flex-direction: column;
-    border:2px solid rgb(31 41 55);
-    border-radius: 4px;
+.card {
+  display: flex;
+  flex-direction: column;
+  border: 2px solid rgb(31 41 55);
+  border-radius: 4px;
 }
-.img-container{
-    padding-top: 8px;
-    border-bottom:2px solid  rgb(31 41 55);
-    max-width:400px
+.img-container {
+  padding-top: 8px;
+  border-bottom: 2px solid rgb(31 41 55);
+  max-width: 400px;
 }
-.img-container img{
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+.img-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
-.text-container{
-    display: flex;
-    flex-direction: column;
-    padding: 12px;
-    text-align: center;
+.text-container {
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  text-align: center;
 }
-.title{
-    font-weight: 500;
-    color: rgba(229 231 235);
-    font-size:14px;
-    line-height:20px;
+.title {
+  font-weight: 500;
+  color: rgba(229 231 235);
+  font-size: 14px;
+  line-height: 20px;
+  text-transform: capitalize;
 }
-.id{
-    color: rgb(156, 163, 175);
-    font-size: 12px;
-    line-height:16px;
+.id {
+  color: rgb(156, 163, 175);
+  font-size: 12px;
+  line-height: 16px;
+}
+
+@media screen and (min-width:768px) {
+    .main-container{
+        grid-template-columns:repeat(3,1fr);
+    }
+}
+@media screen and (min-width:1024px) {
+    .main-container{
+        grid-template-columns:repeat(4,1fr);
+    }
 }
 </style>
