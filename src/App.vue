d71e3303ac0879408801347b09f861af4f9859c2
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-13 11:59:27 +0530

    style upadated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <search />
+  <cards />
 </template>
+
 <script setup>
 import search from "./components/searchBtn.vue";
+import cards from '@/components/cardSection.vue'
 </script>
+
+<style>
+body{
+      background-color:#111827 ;
+    }
+</style>
