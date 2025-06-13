da83327a85e29ecc4f3b5372c725b0f59b65c20e
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-13 15:27:46 +0530

    partial searchfun completion

--- src/components/searchBtn.vue
+++ src/components/searchBtn.vue
@@ -1,27 +1,45 @@
 <template>
-    <section class="main">
-      <div class="search">
-       <form >
-        <input type="text" placeholder="Search ">
-       </form>
-      </div>
-    </section>
-    </template>
+  <section class="main">
+    <div class="search">
+      <form @submit.prevent="search">
+        <input type="text" placeholder="Search" v-model="searchInput" />
+      </form>
+    </div>
+  </section>
+</template>
     
-    <script></script>
+    <script>
+import { reactive } from 'vue';
+
+export default {
+  data() {
+    return {
+      post:''
+    };
+  },
+  methods:{
+    search(){
+     let searchInput=''
+     if(search !=''){
+      console.log(searchInput)
+     }
+    }
+  }
+};
+</script>
     <style>
-    body{
-      background-color:#111827 ;
-    }
-    .search input{
-      padding: 15px;
-      background-color:#ff731f ;
-      border: none;
-      border-radius: 4px;
-    }
-    .search input::placeholder{
-      color: #fff;
-      font-weight: 500;
-      font-size: 16px;
-    }
-    </style>
+body {
+  background-color: #111827;
+}
+.search input {
+  padding: 15px;
+  background-color: #ff731f;
+  border: none;
+  border-radius: 4px;
+}
+.search input::placeholder {
+  color: #fff;
+  font-weight: 500;
+  font-size: 16px;
+}
+</style>
