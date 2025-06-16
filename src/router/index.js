5dc6ce4852b604c2c6c317af7a7c6fed5849dda9
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-16 12:32:03 +0530

    router is added

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter,createWebHistory} from "vue-router";
+
+import serachInput from '@/components/search.vue'
+
+const router=createRouter({
+    history:createWebHistory(import.meta.env.BASE_UR),
+    routes:[
+        {
+            path:'/',
+            path: '/',
+            name: 'search',
+            component:serachInput
+        }
+    ]
+})
+
+export default router
