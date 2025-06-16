e631634b6f235b42ffa4df59ff7436e63f170635
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-16 14:36:57 +0530

    files are formated

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-import { createRouter,createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import serachInput from '@/components/search.vue'
 
-const router=createRouter({
-    history:createWebHistory(import.meta.env.BASE_UR),
-    routes:[
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_UR),
+    routes: [
         {
-            path:'/',
+            path: '/',
             path: '/',
             name: 'search',
-            component:serachInput
+            component: serachInput
         }
     ]
 })
