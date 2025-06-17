ea382eb76f92b01f16b5ee8c920239c446d524ef
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-17 13:33:58 +0530

    vuetify added

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,29 +1,19 @@
 <template>
-  <h2 class="about-title">About</h2>
-  <p class="about-content">
+  <h2 class="text-h3 text-white">About</h2>
+  <p class="text-white text-body-1 text-justify">
     An image is a visual representation of something, while a digital image is a
     binary representation of visual data. These images can take the form of
     photographs, graphics and individual video frames. For this purpose, an
     image is a picture that was created or copied and stored in electronic form.
-    <br />
 
     The details held by a particular image are known as image resolution. The
     higher the resolution, the more visible the details are in an image. Image
     resolution quantifies how close lines can get to each other while remaining
     visibly resolved. Resolution units are tied to physical dimensions, angular
-    subtense or the size of a picture (television lines, or TVL).
-  </p>
+    subtense or the size of a picture (television lines, or TVL).</p>
 </template>
 
 <script></script>
 
 <style scoped>
-.about-title{
-  color: #fff;
-  font-size: 30px;
-}
-.about-content{
-  color: #fff;
-  font-size: 16px;
-}
 </style>
