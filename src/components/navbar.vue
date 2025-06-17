c1cab332c6cbf9060191cebd99fe52629fbc3abf
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-17 08:36:22 +0530

    formated and # added to a tag

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,33 +1,30 @@
 <template>
-  <nav class="gallery-nav">
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="./pages/galleryPage.vue">Gallery</a></li>
-      <li><a href="./pages/AboutPage.vue">About</a></li>
-    </ul>
-  </nav>
+    <nav class="gallary-nav">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
 </template>
 
 <script></script>
 
-<style scoped>
-
-.gallery-nav {
-  background-color: #ff731f;
-  position: fixed;
-  top: 0;
-  left: 0px;
-  width: 100%;
-  padding: 30px;
+<style scoped> 
+.gallary-nav{
+    background-color: #ff731f;
+    position:fixed;
+    top:0;
+    left:0px;
+    width:100%;
+    padding:30px;
 }
-
-.gallery-nav ul {
-  list-style-type: none;
-  color: #fff;
-  display: flex;
-  gap: 20px;
-  font-size: 20px;
-  font-weight: bold;
+.gallary-nav ul{
+    list-style-type:none;
+    color:#fff;
+    display:flex;
+    gap:20px;
+    font-size:20px;
+    font-weight:bold
 }
-
 </style>
