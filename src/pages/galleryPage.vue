55e07dad9e39ba05262916bbe9ead1f13d422d72
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-17 10:49:34 +0530

    gallery Page created

---
+++ src/pages/galleryPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <search />
+    <card-section />
+</template>
+
+<script setup>
+import search from '@/components/search.vue';
+import cardSection from '@/components/cardSection.vue';
+</script>
+
+<style scoped></style>
