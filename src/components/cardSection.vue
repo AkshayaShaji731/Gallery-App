cfa0b7bd4c5c9b537f3a0ab70794d1c6d666a178
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-19 16:10:32 +0530

    card section modified with vuetify

--- src/components/cardSection.vue
+++ src/components/cardSection.vue
@@ -1,16 +1,15 @@
 <template>
-    <section class="gallery-card">
-      <div class="img-container">
-        <img :src="Image" alt="img" />
-      </div>
-      <div class="img-details-container">
-        <span class="img-title">{{ post.title }}</span>
-        <span class="img-id">{{ post.id }}</span>
-      </div>
-    </section>
+    <v-card >
+        <v-img :src="Image" alt="img" />
+      <v-card-item>
+        <v-card-title >{{ props.postData.title }}</v-card-title>
+        <v-card-subtitle >{{ props.postData.id }}</v-card-subtitle>
+      </v-card-item>
+    </v-card>
 </template>
 
 <script setup>
+
 const Image =
   "https://fastly.picsum.photos/id/488/200/200.jpg?hmac=V8mvdG1ON09kNw80-qS00BSFq5gGhqRYoYPJftrsYA8";
 
@@ -20,43 +19,4 @@ const props=defineProps({
 
 </script>
 
-<style>
-.gallery-card {
-  display: flex;
-  flex-direction: column;
-  border: 2px solid #1f2937;
-  border-radius: 4px;
-}
-
-.img-container {
-  padding-top: 8px;
-  border-bottom: 2px solid #1f2937;
-  max-width: 400px;
-}
-
-.img-container img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.img-details-container {
-  display: flex;
-  flex-direction: column;
-  padding: 12px;
-  text-align: center;
-}
-
-.img-title {
-  font-weight: 500;
-  color: #e5e7eb;
-  line-height: 20px;
-  text-transform: capitalize;
-}
-
-.img-id {
-  color: #9ca3af;
-  font-size: 12px;
-  line-height: 16px;
-}
-</style>
+<style scoped></style>
