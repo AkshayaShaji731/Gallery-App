2321998cf760147338549ffa69682b027274dfa7
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-20 15:41:24 +0530

    home style updated

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="pl-8 pr-8 pt-11 pb-11">
-    <h2 class="pa-2  text-white text-h4">Gallery App</h2>
-    <p class="pa-4 text-white text-body-1 ">
+    <h2 class="pa-2  text-white text-h3" >Gallery App</h2>
+    <p class="pa-4 text-white text-body-1 text-justify">
       Welcome to the About Photography Blog, where I share my thoughts and
       essays about photography. Whether you're a beginner or a seasoned veteran,
       there's something on this website that will pique your interest. The site
