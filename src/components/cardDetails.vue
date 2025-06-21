a9ac6aa59b5d13b57868cf586e6d9a16fdeff39e
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-21 12:43:34 +0530

    style updated

--- src/components/cardDetails.vue
+++ src/components/cardDetails.vue
@@ -1,24 +1,22 @@
 <template>
-  <v-container class="my-8" v-for="post in POSTS" :key="post.id">
-     <v-img :src="POST_IMAGE" alt="cardDetail image" class="cover" ></v-img>
-     <p class=" text-center text-white text-h5 py-4 ">{{ post.title }}</p>
-     <p class=" text-center text-white text-h5 py-4 ">{{ post.id }}</p>
+  <v-container class="my-8  d-flex justifiy-center align-center flex-column " v-for="post in POSTS" :key="post.id">
+    <v-img :src="POST_IMAGE" alt="cardDetail image " class="w-25 cover"></v-img>
+    <p class="text-center text-white text-h5 py-4">{{ post.title }}</p>
+    <p class="text-center text-white text-h5 py-4">{{ post.id }}</p>
   </v-container>
 </template>
 
 <script setup>
-import axios from 'axios';
-import { onMounted } from 'vue';
-import { POST_IMAGE ,API_BASE, POSTS} from '@/contant/main';
-
+import axios from "axios";
+import { onMounted } from "vue";
+import { POST_IMAGE, API_BASE, POSTS } from "@/contant/main";
 
 onMounted(async () => {
   try {
     const response = await axios.get(API_BASE);
-    POSTS.value = response.data.filter(item=>item.id<=1);
+    POSTS.value = response.data.filter((item) => item.id <= 1);
   } catch (error) {
     console.error("Error fetching jobs", error);
   }
 });
-
 </script>
