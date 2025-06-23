b04030a84dcb98a6fc18d09fa0cd8cf852678edd
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-23 11:43:59 +0530

    rename component ,model ui editModal created

---
+++ src/components/EditModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition name="model-animation">
+    <div v-show="modalActive" class="modal">
+      <transition name="modal-animation-inner">
+        <i class="far fa-times-circle"></i>
+        <div>
+          <h1>This is modal header</h1>
+          <p>This is a modal message</p>
+        </div>
+        <v-btn>Close</v-btn>
+      </transition>
+    </div>
+  </transition>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
