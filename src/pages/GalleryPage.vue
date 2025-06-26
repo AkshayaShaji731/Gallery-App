3eae0470d9c1a6789b551c930980243d6652db0b
Akshaya Shaji <akshayashaji66@gmail.com>
2025-06-26 20:01:58 +0530

    pinia store add action, modified gallery and postsItem

--- src/pages/GalleryPage.vue
+++ src/pages/GalleryPage.vue
@@ -23,11 +23,12 @@
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 
 import cardSection from "@/components/Card.vue";
 import searchBar from "@/components/SearchInput.vue";
 import { usePostStore } from "@/stores/PostStore";
+import { DISPLAY_CARDS_COUNT } from "@/constant";
 
 const postStore = usePostStore();
 const searchInput = ref("");
@@ -62,7 +63,11 @@ onMounted(async () => {
   }
 });
 
-postStore.postFetch();
+onMounted(async () => {
+  const post = await postStore.postFetch();
+  const data = post.filter((item) => item.id <= DISPLAY_CARDS_COUNT);
+  postStore.updatePost(data);
+});
 </script>
 
 <style scoped>
--- src/pages/PostItem.vue
+++ src/pages/PostItem.vue
@@ -4,39 +4,44 @@
       <v-btn class="bg-orange-accent-4">Back</v-btn></RouterLink
     >
     <EditModal @close="onClose" @save="onSave" :modal-active="modalActive">
-      <div
-        class="d-flex flex-column px-8"
-        v-for="post in postItemStore.postValue"
-        :key="post.id"
-      >
-        <v-text-field v-model="post.id" readonly></v-text-field>
-        <v-text-field v-model="post.title"></v-text-field>
+      <div class="d-flex flex-column px-8">
+        <v-text-field v-model="editForm.id" readonly></v-text-field>
+        <v-text-field v-model="editForm.title"></v-text-field>
         <v-text-field v-model="POST_IMAGE"></v-text-field>
       </div>
     </EditModal>
     <v-btn class="bg-orange-accent-4" @click="onClose">Edit</v-btn>
   </div>
-  <v-container
-    class="my-8 d-flex justifiy-center align-center flex-column"
-    v-for="post in postItemStore.postValue"
-    :key="post.id"
-  >
+  <v-container class="my-8 d-flex justifiy-center align-center flex-column">
     <v-img :src="POST_IMAGE" alt="cardDetail image" class="w-50 cover"></v-img>
-    <p class="text-center text-white text-h5 py-4">{{ post.title }}</p>
-    <p class="text-center text-white text-h5 py-4">{{ post.id }}</p>
+    <p class="text-center text-white text-h5 py-4">
+      {{ postValue[0].title }}
+    </p>
+    <p class="text-center text-white text-h5 py-4">
+      {{ postValue[0].id }}
+    </p>
   </v-container>
 </template>
 
 <script setup>
 import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 
 import { POST_IMAGE } from "@/constant/index";
 import EditModal from "@/components/EditModal.vue";
 import { usePostStore } from "@/stores/PostStore";
+import { DISPLAY_CARDS_COUNT } from "@/constant";
+
 
 const postItemStore = usePostStore();
 const modalActive = ref(false);
-
+const route = useRoute();
+const postItem = route.params.id;
+const postValue = postItemStore.posts.filter((item) => item.id == postItem);
+const editForm = {
+  id: " ",
+  title: ""
+};
 const onClose = () => {
   modalActive.value = !modalActive.value;
 };
@@ -45,7 +50,11 @@ const onSave = () => {
   alert("post updated");
 };
 
-onMounted(() => {
-  postItemStore.postItemFetch();
+onMounted(async () => {
+  const post = await postItemStore.postFetch();
+  const data = post.filter((item) => item.id <= DISPLAY_CARDS_COUNT);
+  postItemStore.updatePost(data);
+  editForm.id= postValue[0].id;
+  editForm.title= postValue[0].title;
 });
 </script>
